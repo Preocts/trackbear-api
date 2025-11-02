c400ad2288e0d1a00217f33048f793b090ca55ce
Preocts <preocts@preocts.com>
2025-11-02 15:06:17 -0500

    Implement GET request with custom response model

---
+++ src/trackbear_api/_trackbearresponse.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import Any
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class TrackBearResponse:
+    """
+    TrackBear API response Model.
+
+    Always check `success` before processing additional attributes.
+
+    When `success` is True: `data` will be available for processing
+
+    When `success` is False: `code` and `message` will be available for processing
+    """
+
+    success: bool = False
+    data: Any = ""
+    code: str = ""
+    message: str = "Undefined Model"
