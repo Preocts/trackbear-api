71a5cdb3e5d558c5a8da265c946feee71130ebc2
Preocts <preocts@preocts.com>
2025-11-02 14:21:50 -0500

    Refactor and clean tests

--- tests/trackbearclient_test.py
+++ tests/trackbearclient_test.py
@@ -7,28 +7,6 @@ import pytest
 from trackbear_api import TrackBearClient
 
 
-@pytest.mark.usefixtures("add_environ_token")
-def test_init_client_providing_keyword_token() -> None:
-    """
-    Initialize the client, providing the API token directly.
-
-    The set environ token should be ignored.
-    """
-    client = TrackBearClient(api_token="keyword_value")
-
-    assert client.session.headers["Authorization"] == "Bearer keyword_value"
-
-
-@pytest.mark.usefixtures("add_environ_token")
-def test_init_client_providing_environ_token() -> None:
-    """
-    Initialize the client, providing the API token by the environment
-    """
-    client = TrackBearClient()
-
-    assert client.session.headers["Authorization"] == "Bearer environ_value"
-
-
 def test_init_client_providing_no_token() -> None:
     """
     Initialize the client without a token. Expect an exception raised.
@@ -39,77 +17,51 @@ def test_init_client_providing_no_token() -> None:
         TrackBearClient()
 
 
-@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent")
-def test_init_client_user_agent_custom() -> None:
+@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent", "add_environ_url")
+def test_init_client_custom_values() -> None:
     """
-    Initialize the client, providing a custom user-agent.
+    Initialize the client, providing a custom keyword values
 
-    Expected to override the environ user-agent.
+    Expected to override the environ values
     """
-    expected_value = "test agent/1.0.0"
+    expected_api_token = "mock_api_key"
+    expected_url = "https://some.other.app"
+    expected_user_agent = "my custom app/1.0"
 
-    client = TrackBearClient(user_agent=expected_value)
+    client = TrackBearClient(
+        api_token=expected_api_token,
+        api_url=expected_url,
+        user_agent=expected_user_agent,
+    )
 
-    assert client.session.headers["User-Agent"] == expected_value
+    assert client.session.headers["Authorization"] == f"Bearer {expected_api_token}"
+    assert client.api_url == expected_url
+    assert client.session.headers["User-Agent"] == expected_user_agent
 
 
-@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent")
-def test_init_client_user_agent_environ() -> None:
+@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent", "add_environ_url")
+def test_init_client_environ_values() -> None:
     """
-    Initialize the client, assert environment provided agent is used.
+    Initialize the client, assert environment values provided are used
     """
     expected_value = "environ_value"
 
     client = TrackBearClient()
 
+    assert client.session.headers["Authorization"] == f"Bearer {expected_value}"
+    assert client.api_url == expected_value
     assert client.session.headers["User-Agent"] == expected_value
 
 
 @pytest.mark.usefixtures("add_environ_token")
-def test_init_client_user_agent_default() -> None:
+def test_init_client_default_values() -> None:
     """
-    Initialize the client, assert default agent is used.
+    Initialize the client, assert default values are used. Excludes API token.
     """
-    expected_value = f"trackbear-api/{importlib.metadata.version('trackbear-api')} (https://github.com/Preocts/trackbear-api) by Preocts"
+    expected_url = "https://trackbear.app/api/v1/"
+    expected_user_agent = f"trackbear-api/{importlib.metadata.version('trackbear-api')} (https://github.com/Preocts/trackbear-api) by Preocts"
 
     client = TrackBearClient()
 
-    assert client.session.headers["User-Agent"] == expected_value
-
-
-@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent", "add_environ_url")
-def test_init_client_api_url_custom() -> None:
-    """
-    Initialize the client, providing a custom api url.
-
-    Expected to override the environ api url.
-    """
-    expected_value = "https://some.other.app"
-
-    client = TrackBearClient(api_url=expected_value)
-
-    assert client.api_url == expected_value
-
-
-@pytest.mark.usefixtures("add_environ_token", "add_environ_useragent", "add_environ_url")
-def test_init_client_api_url_environ() -> None:
-    """
-    Initialize the client, assert environment provided url is used.
-    """
-    expected_value = "environ_value"
-
-    client = TrackBearClient()
-
-    assert client.api_url == expected_value
-
-
-@pytest.mark.usefixtures("add_environ_token")
-def test_init_client_api_url_default() -> None:
-    """
-    Initialize the client, assert default url is used.
-    """
-    expected_value = "https://trackbear.app/api/v1/"
-
-    client = TrackBearClient()
-
-    assert client.api_url == expected_value
+    assert client.api_url == expected_url
+    assert client.session.headers["User-Agent"] == expected_user_agent
