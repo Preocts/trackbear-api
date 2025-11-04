a9471432ccbc5a60b68ec9f50864f44dc8412d8f
Preocts <preocts@preocts.com>
2025-11-04 16:16:36 -0500

    Add exceptions to public API of library

--- src/trackbear_api/__init__.py
+++ src/trackbear_api/__init__.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 from ._trackbearresponse import TrackBearResponse
 from .enums import Phase
 from .enums import State
+from .exceptions import APIResponseError
+from .exceptions import ModelBuildError
 from .trackbearclient import TrackBearClient
 
 __all__ = [
+    "APIResponseError",
+    "ModelBuildError",
     "Phase",
     "State",
     "TrackBearClient",
