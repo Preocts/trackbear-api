3cd71b445da6d4b60c50fe603fb1a21eeec312f0
Preocts <preocts@preocts.com>
2025-11-08 00:41:01 -0500

    Implement TallyClient.save()

--- src/trackbear_api/_tallyclient.py
+++ src/trackbear_api/_tallyclient.py
@@ -42,6 +42,7 @@ class TallyClient:
 
         Raises:
             APIResponseError: On any failure message returned from TrackBear API
+            ValueError: When a given parameter is an invalid value.
         """
         # Forcing the use of the Enum here allows for fast failures at runtime if the
         # incorrect string is provided.
@@ -90,6 +91,70 @@ class TallyClient:
             APIResponseError: On failure to retrieve requested model
         """
         response = self._api_client.get(f"/tally/{tally_id}")
+
+        if not response.success:
+            raise APIResponseError(
+                status_code=response.status_code,
+                code=response.error.code,
+                message=response.error.message,
+            )
+
+        return Tally.build(response.data)
+
+    def save(
+        self,
+        work_id: int,
+        date: str,
+        measure: Measure | str,
+        count: int,
+        note: str = "",
+        tags: Sequence[str] | None = None,
+        tally_id: int | None = None,
+        *,
+        set_total: bool = False,
+    ) -> Tally:
+        """
+        Save a Tally.
+
+        If `tally_id` is provided, then the existing tally is updated. Otherwise,
+        a new tally is created.
+
+        Args:
+            work_id (int): ID of the project the tally is applied to
+            date (str): Date of the tally (YYYY-MM-DD)
+            measure (Measure | str): Measure enum of the following: `word`, `time`,
+                `page`, `chapter`, `scene`, or `line`.
+            count (int): Value of the measure
+            note (str): A note for the tally
+            tags (Sequence[str]): A list of tag names to apply. New tags will be created.
+            tally_id (int): Existing project id if request is to update existing projects
+            set_total (bool): If true, the provided count will be set as the project total.
+
+        Returns:
+            Tally object on success
+
+        Raises:
+            APIResponseError: On any failure message returned from TrackBear API
+            ValueError: When a given parameter is an invalid value.
+        """
+        # Forcing the use of the Enum here allows for fast failures at runtime if the
+        # incorrect string is provided.
+        measure = measure if isinstance(measure, Measure) else Measure(measure)
+
+        payload = {
+            "date": date,
+            "measure": measure.value,
+            "count": count,
+            "note": note,
+            "workId": work_id,
+            "setTotal": set_total,
+            "tags": tags or [],
+        }
+
+        if tally_id is None:
+            response = self._api_client.post("/tally", payload)
+        else:
+            response = self._api_client.patch(f"/tally/{tally_id}", payload)
 
         if not response.success:
             raise APIResponseError(
